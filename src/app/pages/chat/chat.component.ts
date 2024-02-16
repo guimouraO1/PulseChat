9521ff127b5475210e4a9c2b9e9453faab777115
guimouraO1 <guimoura.oliveira@gmail.com>
2024-02-16 18:23:12 -0300

    styling chat

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, MatIconModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
