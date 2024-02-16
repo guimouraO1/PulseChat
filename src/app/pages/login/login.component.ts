81e6294b4e1efb9ba2e05b1719c88ad030678ebf
guimouraO1 <guimoura.oliveira@gmail.com>
2024-02-16 17:30:10 -0300

    stiling login and adding validations

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,16 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatButtonModule, MatInputModule, MatIconModule, MatTooltipModule, MatFormFieldModule],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    MatTooltipModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-  // this.buttonToggleMessage = 'Do you already have an account? Click here';
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private _snackBar: MatSnackBar
+  ) {}
+
+  openSnackBar() {
+    this._snackBar.open('Complete all the fields correctly.', 'I understood!', {
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+      duration: 8000,
+    });
+  }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['',[ Validators.required, Validators.maxLength(30)]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Form submitted:', this.loginForm.value);
+    } else {
+      this.openSnackBar();
+    }
+  }
 }
